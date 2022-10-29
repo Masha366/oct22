7a21986670db10edbda7fa8dd6b9e1f619527b5a
Maria Kalinina <muhaklinika@gmail.com>
2022-10-29 11:25:27 +0300

    task1

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def function1 (*args):
-    l=len(args)
-    s=0
-    c=0
-    while c<l:
-       s=s+args[c]
-       c=c+1
-    return s
-
+        l=len(args)
+        s=0
+        c=0
+        while c<l:
+            if type(args[c]) == int or type(args[c]) == float:
+                s=s+args[c]
+                c=c+1
+            else:
+                return "Error"
+        return s
+print(function1(1, 3, 10))
 
 
 
